a87c96f10051b21882fb7ecaab8b71ace281a9d9
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-07-14 13:19:38 -0400

    setup: build package from subdirectory
    This is currently failing. See https://github.com/scikit-build/scikit-build/issues/335

--- swig/python/setup.py
+++ swig/python/setup.py
@@ -10,5 +10,6 @@ setup(
     packages=['replicate_pytime_timeval_error'],
     tests_require=[],
     setup_requires=[],
-    cmake_args=['-DPYTHON_REL_SITE_ARCH:PATH=replicate_pytime_timeval_error']
+    cmake_args=['-DPYTHON_REL_SITE_ARCH:PATH=replicate_pytime_timeval_error'],
+    cmake_source_dir="../../"
 )
